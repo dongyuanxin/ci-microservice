49644e907cca365889e939e83b6cbc4634010ad9
yuanxindong <yuanxin.me@gmail.com>
2020-01-05 10:40:26 +0800

    config: global vars

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    AUTH_PATH: process.env.AUTH_KEY || 'test',
+    BLOG_GIT: 'git@git.dev.tencent.com:godbmw/xxoo521-template.git',
+    BLOG_FOLDER: '/Users/yuanxindong/Desktop/articles/ci-xxoo521/tmp/xxoo521',
+    POST_GIT: 'git@git.dev.tencent.com:godbmw/xxoo521-posts.git',
+    POST_FOLDER: '/Users/yuanxindong/Desktop/articles/ci-xxoo521/tmp/xxoo521/source/_posts',
+    ROOT_FOLDER: '/Users/yuanxindong/Desktop/articles/ci-xxoo521/tmp/build/'
+}
